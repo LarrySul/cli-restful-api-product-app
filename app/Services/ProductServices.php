f202f2ac37f07c67ab3a3e5bb8a885ef196decd3
Olanrewaju <olanrewaju@seamlesshr.com>
2022-11-19 21:26:26 +0100

    create read product data from csv method

--- app/Services/ProductServices.php
+++ app/Services/ProductServices.php
@@ -22,5 +22,42 @@ class ProductServices
         }
     }
 
-    
+    public function readProductDataFromCsv($product_data_path)
+    {
+        try {
+            $product_array = [];
+            if (($open = fopen($product_data_path, "r")) !== FALSE) 
+            {
+                while (($data = fgetcsv($open, 1000, ",")) !== FALSE) 
+                {       
+                    $data = [
+                            'sku' => $data[0],
+                            'name' => $data[1],
+                            'category' => $data[2],
+                            'price' => $data[3]
+                    ]; 
+
+                    $product_array[] = $data; 
+                }
+                fclose($open);
+            }
+            array_shift($product_array);
+            
+            $row_chunk = array_chunk($product_array, 500);
+            $chunk_product_data = array_map(function($row_chunk, $keys){
+                return new CreateProductJob($row_chunk);
+              }, $row_chunk, array_keys($row_chunk));
+
+            $batch = Bus::batch($chunk_product_data)->then(function (Batch $batch) {
+                info('all jobs completed successfully...');
+            })->catch(function (Batch $batch, Throwable $e) {
+                info('first batch job failure detected...'. json_encode($e->getMessage()));
+            })->finally(function (Batch $batch) {
+                info('batch executed successful');
+            })->dispatch();
+
+        } catch (Throwable $e) {
+            info('Error occured '.json_encode($e->getMessage()));
+        }
+    }
 }
