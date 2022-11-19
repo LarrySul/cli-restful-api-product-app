353328e33492f3f1164d470744b7730881550152
Olanrewaju <olanrewaju@seamlesshr.com>
2022-11-19 21:36:01 +0100

    set up migration and config files

---
+++ config/shopping-list.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+    "products" => [
+        [ "sku" => "000001",
+            "name" => "BV Lean leather ankle boots",
+            "category" => "boots",
+            "price" => "89.00€"
+        ],
+        [
+            "sku" => "000002",
+            "name" => "BV Lean leather ankle boots",
+            "category" => "boots",
+            "price" => "99.00€"
+        ],
+        [
+            "sku"=> "000003",
+            "name"=> "Ashlington leather ankle boots",
+            "category"=> "boots",
+            "price"=> "71.00€"
+        ],
+        [
+            "sku" => "000004",
+            "name" => "Naima embellished suede sandals",
+            "category" => "sandals",
+            "price" => "795.00€"
+        ],
+        [
+            "sku" => "000005",
+            "name" => "Nathane leather sneakers",
+            "category" => "sneakers",
+            "price" => "59.00€"
+        ],
+    ]
+];
